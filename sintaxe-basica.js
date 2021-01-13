6af065b765b7e7585540f71586efc8a4f9757f69
isacfarias <isac.farias.21@gmail.com>
2021-01-12 21:26:55 -0400

    dados, variaveis, camelCase

---
+++ sintaxe-basica.js
@@ -0,0 +1,49 @@
+//statemantes termina com ponto e virgula(;)
+var acelora = 2;
+
+//valores: literais e variaveis
+
+//literais
+
+10;
+10.5;
+
+'estuda que rapadura é doce mas não é mole';
+"estuda que rapadura é doce mas não é mole";
+
+//variaveis
+
+var aTalVariavel;
+
+aTalVariavel = 10;
+console.log(aTalVariavel);
+
+
+//operadores
+console.log('Soma:', 10 + 5);
+
+console.log('subtração:', 10 - 5);
+
+console.log('Mutiplicação:', 10 * 5);
+
+console.log('Divisão:', 10 / 5);
+
+
+//Identificadores padrão camelCase
+
+var c233xcsdf;
+
+var _fdfd;
+var _2fdfd;
+
+var $1sdsd;
+var $asdsd;
+
+var saldoCompra;
+
+//não se deve inciar nome de variaveis com numeros
+
+var 3abc;
+
+
+
