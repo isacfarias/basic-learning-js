a8d31b361131bb80ec380032d8627f309aa3b0c4
isacfarias <isac.farias.21@gmail.com>
2021-01-13 21:47:46 -0400

    conceitos de data

---
+++ date.js
@@ -0,0 +1,8 @@
+var data = new Date();//real time
+console.log(data);
+
+data = new Date('07/01/2021');//mm-dd-yyyy
+console.log(data);
+
+data = new Date('07/01/2021 17:25:56');//mm-dd-yyyy
+console.log(data);
