1262aee0fe6595d86415001ee847738716465afb
isacfarias <isac.farias.21@gmail.com>
2021-01-13 21:42:03 -0400

    conceito de arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var obj = {
+    comida:"arroz",
+    proteina:"carne de frango"
+}
+
+//java script não é tipado
+var graos = ["arroz", "feijão", "lentilha", "milho", "trigo"];
+console.log(graos[1]);
+//pode ter varias tipos de dados em um arrei
+var obj2 = ["cachoeira", 23, ["a não véi", "qualaquer coisa"]];
+
