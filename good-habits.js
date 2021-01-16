2de001e603043e60f6ba14c291bc249e0187d3d0
isacfarias <isac.farias.21@gmail.com>
2021-01-16 11:14:56 -0400

    algumas boas praticas para organiçao de codigos para

---
+++ good-habits.js
@@ -0,0 +1,22 @@
+(function() {
+
+    var escopoLocal,
+    escopoGlocal,
+    foo,
+    obj,
+    array;
+
+    escopoLocal = "chocolate";//ideal é sempre usar apas duplas para string
+    console.log(escopoLocal);
+
+    obj = {nome:"josé",sobrenome:"dos santos"};//alguns compiladores não são compativies com aspa simples ou apostrofo, portanto o ideal pe uma aspa duplas
+
+    array = [
+        "framboesa",
+        "morango",
+        "banana"
+    ];
+
+
+
+})();
